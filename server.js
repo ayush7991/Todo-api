74b6524c17bdff09df7013334097c975c7c5f0e7
silentwraith <iamdictator1997@gmail.com>
2016-12-26 19:24:35 +0530

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
@@ -56,6 +57,37 @@ app.delete('/todos/:id',function(req,res){
 	else{
 		res.status(404).send();
 	}
+});
+
+app.put('/todos/:id', function(req,res){	
+	var todoId = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description' , 'completed');
+	//var todoId = parseInt(req.params.id,10);
+	var validAttributes = {};
+
+	if(!matchedTodo){
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	}
+	else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description)&& body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	}
+	else if(body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+
+
+	_.extend(matchedTodo,validAttributes);
+	res.json(matchedTodo);
+
 });
 
 app.listen(PORT,function(){
