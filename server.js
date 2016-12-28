cc9074f303e49f25856fd7effba1f7fe668fb63f
silentwraith <iamdictator1997@gmail.com>
2016-12-28 18:17:15 +0530

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -16,44 +16,44 @@ app.get('/', function(req, res) {
 app.get('/todos', function(req, res) {
 	var query = req.query;
 	var where = {};
-	if(query.hasOwnProperty('completed') && query.completed === 'true'){
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
-	}
-	else if(query.hasOwnProperty('completed') && query.completed === 'false'){
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
 		where.completed = false;
 	}
 
-	if(query.hasOwnProperty('q') && query.q.length > 0){
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
 		where.description = {
 			$like: '%' + query.q + '%'
 		}
 	}
-	db.todo.findAll({where: where}).then(function(todos){
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		res.json(todos);
-	},function(e){
+	}, function(e) {
 		res.status(500).send();
 	})
 })
 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	db.todo.findById(todoId).then(function(todo){
-		if(todo){
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
 			res.json(todo.toJSON());
-		}
-		else{
+		} else {
 			res.status(404).send();
 		}
-	},function(e){
+	}, function(e) {
 		res.status(500).send();
 	})
 })
 
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	db.todo.create(body).then(function(todo){
+	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	},function(e){
+	}, function(e) {
 		res.status(400).json(e);
 	})
 });
@@ -61,16 +61,25 @@ app.post('/todos', function(req, res) {
 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted){
+		if(rowsDeleted === 0){
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		}
+		else{
+			res.status(204).send();
+		}
+	},function(){
+		res.status(500).send();
 	});
 
-	if (matchedTodo) {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+
 });
 
 app.put('/todos/:id', function(req, res) {
